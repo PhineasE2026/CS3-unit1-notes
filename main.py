4ad7db66bfb71f996ecc1919e8f9c20685816ec0
finny <phineas.ehrlich2026@gbwl.org>
2025-09-12 13:40:21 +0000

    topic 1.1 the notes

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
-print("hamorphavorsche")
+print("Gorb")
 
-# Variables store data. Variables
-lucky_num = 37 # int
-my_name = "Finny" # str
-cash = 5.50 # float
-is_raining = True #Booleandatatype
-is_sunny = False #Glorpshmorp
+lucky_num = 37
+my_name = "Car Sifflington"
+cash = 5.50
+is_raining = True
+is_sunny = False
 
+greeting = "Howdy, " + my_name
+print(greeting)
+
+greeting = 'Shalom yeled, Ani akshev\' barukh ata adonai'
+print(greeting)
+
+long_greeting = """
+                Oh shmeep shmorp gleep atanadeep
+                In western lands there stood a sheep
+                Leaping round the evergreens
+                Moschep alep gatanamep
+                """
+
+print(long_greeting)
+
+# f-strings are FORMATTED strings (like templates)
+f_string = f"Hello my name is {my_name} and i pay with {cash}"
+print(f"Hello my name is {f_string} and i pay with {cash}")
+
+f_string2 = "Hibiscus Tea"
+
+def HibiscusTeaSimulation():
+    #function body indented under the colon
+    print("Hello Hisibsuvc Tea iam functional")
+    print("Habisucs tea")
+    print("hibisucs")
+
+HibiscusTeaSimulation()
+
+def multiply(x, y):
+    return x * y
+
+print(multiply(150230, 403201))
+
+five_times_three = multiply(5, 3)
+print(five_times_three)
+
+one_hundred_seventy_five_billion_times_three_thousand_sixty_five_plus_seven_to_the_power_of_3 = (multiply(175000000000,3365) + (7 ** 3))
+
+print(one_hundred_seventy_five_billion_times_three_thousand_sixty_five_plus_seven_to_the_power_of_3)
+
+print(multiply(20, 6))
